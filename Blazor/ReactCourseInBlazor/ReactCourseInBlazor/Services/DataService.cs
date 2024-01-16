99ed36d07d7ea3a7105c190233e4ca46d2236c43
mihaelfi <mihaelfi@gmail.com>
2024-01-16 20:18:26 +0200

    Add Lecture 02 solution pages and update DataService
    Three new solution pages for Lecture 02 have been added: Home, TaskOneSolution, and TaskTwoSolution. When the pages are initialized, they change the title of the lecture in the DataService, triggering a re-render of the UI elements dependent on this service. A new layout for these solution pages has been created for consistency.

--- Blazor/ReactCourseInBlazor/ReactCourseInBlazor/Services/DataService.cs
+++ Blazor/ReactCourseInBlazor/ReactCourseInBlazor/Services/DataService.cs
@@ -4,5 +4,16 @@ namespace ReactCourseInBlazor.Services;
 
 public class DataService
 {
+    public event Action OnChange;
+    private void NotifyStateChanged() => OnChange?.Invoke();
     public ContactUsFormModel FormModel { get; set; } = new();
+
+    public string? LectureTwoSolutionTitle { get; private set; }
+
+    public void ChangeLectureTwoTitle(string title)
+    {
+        LectureTwoSolutionTitle = title;
+        Console.WriteLine($"[DataService]: Setting title to {title}");
+        NotifyStateChanged();
+    }
 }
