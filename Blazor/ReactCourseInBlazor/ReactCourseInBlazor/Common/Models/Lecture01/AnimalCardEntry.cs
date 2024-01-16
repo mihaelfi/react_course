a054ad5e71bb63c95fa092ae08d1eb647b108fe6
mihaelfi <mihaelfi@gmail.com>
2024-01-16 19:28:20 +0200

    Add AnimalCardEntry model and refactor Animals.razor
    A new model class 'AnimalCardEntry' has been added to encapsulate the properties of an animal card entry. The Animals.razor page has been refactored to use this model instead of the previously inline-defined CardEntry class, tidying up the code and promoting reusability.

---
+++ Blazor/ReactCourseInBlazor/ReactCourseInBlazor/Common/Models/Lecture01/AnimalCardEntry.cs
@@ -0,0 +1,8 @@
+namespace ReactCourseInBlazor.Common.Models.Lecture01;
+
+public class AnimalCardEntry
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImageSrc { get; set; }
+}
